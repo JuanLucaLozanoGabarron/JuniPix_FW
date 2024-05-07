5449bdad0028f5f2abccf4d95ac19d6b44d17277
JuanLucaLozanoGabarron <juan-luca.lozano.gabarron@student.ehb.be>
2024-05-07 11:33:17 +0200

    Add a new route in index.js + added css to homepage

--- junipix/src/index.js
+++ junipix/src/index.js
@@ -5,6 +5,7 @@ import Homepage from "./routes/Homepage";
 import reportWebVitals from "./reportWebVitals";
 import Background from "./components/Background";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Artwork from "./routes/Artwork";
 <style>
   @import
   url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Homepage />,
+  },
+  {
+    path: "/artworks",
+    element: <Artwork />,
   },
 ]);
 
