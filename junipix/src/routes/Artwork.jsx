ba3501a28a8b08608c405a2f35d169ac812de812
JuanLucaLozanoGabarron <juan-luca.lozano.gabarron@student.ehb.be>
2024-05-07 13:50:01 +0200

    Changed some css on homepage and artwork page

--- junipix/src/routes/Artwork.jsx
+++ junipix/src/routes/Artwork.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Header from "../components/Header";
+import "./style/artwork.css";
+import Card from "../components/Card";
 
 export default function Artwork() {
   return (
     <>
       <Header />
-      
+      <div className="searchPage">
+        <div className="titleOfSearch">
+          <h1>Search</h1>
+        </div>
+        <div className="cardsOfArtwortk">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
     </>
   );
 }
