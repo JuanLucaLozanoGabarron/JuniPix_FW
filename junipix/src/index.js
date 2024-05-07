eee2283f702c2a66f3cb687ac639cc8e303280dd
JuanLucaLozanoGabarron <juan-luca.lozano.gabarron@student.ehb.be>
2024-05-07 11:04:14 +0200

    Add react router dom to project

--- junipix/src/index.js
+++ junipix/src/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Homepage from "./routes/Homepage";
+import reportWebVitals from "./reportWebVitals";
+import Background from "./components/Background";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+  @import
+  url('https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&display=swap');
+</style>;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    <Background />
   </React.StrictMode>
 );
 
