d0d095fbcb0937f579fc3c3912fbfea0a30ac022
JuanLucaLozanoGabarron <juan-luca.lozano.gabarron@student.ehb.be>
2024-05-31 10:05:00 +0200

    Create ai images generator and add auth in .env file (add dotenv for webpack)

--- junipix/src/routes/CreateAI.jsx
+++ junipix/src/routes/CreateAI.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Header from "../components/Header";
-import AI from "../components/AI";
+import Art from "../components/images/hand.jpeg";
+import Heart from "../components/images/heart.png";
+import AI from "../components/AI.jsx";
 import "./style/createAi.css";
 
 export default function CreateAI() {
+  const [images, setImages] = useState([]);
+  let inputRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+
+  const auth = {
+    apiKey: `${process.env.REACT_APP_MY_API_KEY}`,
+    api: `${process.env.REACT_APP_MY_API}`,
+  };
+
+  const imageGenerator = async () => {
+    if (inputRef.current.value === "") {
+      return 0;
+    }
+    setLoading(true);
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.REACT_APP_MY_API_KEY}`,
+          "User-Agent": "Chrome",
+        },
+        body: JSON.stringify({
+          prompt: `${inputRef.current.value}`,
+          n: 4,
+          size: "256x256",
+        }),
+      }
+    );
+    let data = await response.json();
+    console.log(data);
+    setImages(data.data);
+    setLoading(false);
+  };
+
   return (
     <>
       <Header />
@@ -14,19 +52,34 @@ export default function CreateAI() {
           <hr />
         </div>
         <div className="searchAi">
-          <img src="" alt="" />
           <input
             type="text"
+            ref={inputRef}
             placeholder="Describe the image you wish to create"
           />
-          <button>Generate</button>
+          <button onClick={() => imageGenerator()}>Generate</button>
         </div>
-        <button id="regenerate">Regenerate</button>
+        <div className="loading">
+          <div className={loading ? "loading-bar-full" : "loading-bar"}></div>
+        </div>
+
         <div className="cardsOfAi">
-          <AI />
-          <AI />
-          <AI />
-          <AI />
+          {images.map((image, index) => (
+            <div className="cardAi">
+              <div className="likeAi">
+                <button>
+                  <img src={Heart} alt="" />
+                </button>
+              </div>
+              <div className="imageAi">
+                <img
+                  key={index}
+                  src={image.url === "/" ? Art : image.url}
+                  alt=""
+                />
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
