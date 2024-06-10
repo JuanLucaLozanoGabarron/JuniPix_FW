fcbfc4b21618ae85d3e8559dc8c64499f911f268
JuanLucaLozanoGabarron <juan-luca.lozano.gabarron@student.ehb.be>
2024-06-11 00:43:22 +0200

    Add new component with information + change width of images + remove background

--- junipix/src/routes/Homepage.jsx
+++ junipix/src/routes/Homepage.jsx
@@ -3,12 +3,13 @@ import Header from "../components/Header";
 import { Link } from "react-router-dom";
 import GoAi from "./images/goAi.png";
 import { Gallery } from "react-grid-gallery";
+import Information from "../components/Information";
 
-function App() {
+export default function App() {
   const images1 = [
     {
       src: "https://i.postimg.cc/NfsnYDnL/temp-Image-QFHUg-V.avif",
-      width: 180,
+      width: 190,
       height: 190,
     },
     {
@@ -20,12 +21,12 @@ function App() {
   const images2 = [
     {
       src: "https://i.postimg.cc/VvYKfsD2/temp-Image-Xl-PCur.avif",
-      width: 235,
+      width: 240,
       height: 190,
     },
     {
       src: "https://i.postimg.cc/XYTxZksf/temp-Imageqt9z-SJ.avif",
-      width: 235,
+      width: 240,
       height: 190,
     },
   ];
@@ -37,15 +38,15 @@ function App() {
     },
     {
       src: "https://i.postimg.cc/X7qgQzhK/temp-Image-YQ1-GVv.avif",
-      width: 180,
+      width: 190,
       height: 190,
     },
   ];
 
   return (
-    <div className="background">
-      <div className="App">
-        <Header />
+    <>
+      <Header />
+      <div className="homepage">
         <div className="firstInfo">
           <div className="text">
             <div className="title">
@@ -68,9 +69,8 @@ function App() {
             <Gallery images={images3} />
           </div>
         </div>
+        <Information />
       </div>
-    </div>
+    </>
   );
 }
-
-export default App;
